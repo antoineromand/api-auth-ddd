5647b41bc63bab00672485168560b920ee76fe96
Antoine Romand <antoine.romand@epitech.eu>
2025-04-13 19:07:58 +0200

    feat: configure a filter before some request (private/api/..), extract from header id and role + create authentication custom (store id and role)

--- src/main/java/com/dxs/auth/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/dxs/auth/infrastructure/config/SecurityConfig.java
@@ -1,23 +1,78 @@
 package com.dxs.auth.infrastructure.config;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Value("${GATEWAY_URI}")
+    private static String gatewayUri;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .authorizeHttpRequests(auth -> auth.
+                        requestMatchers("/public/api/v1/auth")
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(new CustomHeaderFilter(), UsernamePasswordAuthenticationFilter.class)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable);
         return http.build();
+    }
+
+    public static class CustomHeaderFilter extends OncePerRequestFilter {
+
+        @Override
+        protected void doFilterInternal(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        FilterChain filterChain) throws ServletException, IOException {
+
+            String uri = request.getRequestURI();
+
+            if(uri.startsWith("private/api")) {
+                String origin = request.getHeader("Origin");
+                String userId = request.getHeader("x-user-id");
+                String userRole = request.getHeader("x-user-role");
+
+                if (!gatewayUri.equals(origin)
+                        || userId == null || userRole == null) {
+                    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                    response.getWriter().write("Forbidden: Missing or invalid headers");
+                    return;
+                }
+
+                List<SimpleGrantedAuthority> authorities =
+                        List.of(new SimpleGrantedAuthority("ROLE_" + userRole.toUpperCase()));
+
+                Authentication auth =
+                        new UsernamePasswordAuthenticationToken(userId, null, authorities);
+
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+
+            filterChain.doFilter(request, response);
+        }
     }
 }
